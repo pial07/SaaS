f560ffec4a3753cfeafc48fa4ffaad54bd45501c
pial07 <haider.powergen@gmail.com>
2025-02-27 15:27:27 +0600

    updated commit

--- src/home/views.py
+++ src/home/views.py
@@ -5,13 +5,13 @@ def home_page_view(request, *args, **kwargs):
     qs= PageVisit.objects.all()
     page_qs= PageVisit.objects.filter(path=request.path)
     my_title= "Hello Pial"
+    html_template= "home.html"
     context= {"title": my_title, 
               "qs": qs.count(),
               "page_visits": page_qs.count(),
               "total_visits": qs.count(),
               "percent": (page_qs.count()*100)/qs.count(),
               }
-    html_template= "home.html"
     path=request.path
 
     PageVisit.objects.create(path=request.path)
