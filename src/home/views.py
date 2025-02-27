f6659ff30d311621702de49719798cb4d0ae3ce6
pial07 <haider.powergen@gmail.com>
2025-02-27 13:56:59 +0600

    initial SaaS

---
+++ src/home/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from visits.models import PageVisit
+
+def home_page_view(request, *args, **kwargs):
+    qs= PageVisit.objects.all()
+    page_qs= PageVisit.objects.filter(path=request.path)
+    my_title= "Hello Pial"
+    context= {"title": my_title, 
+              "qs": qs.count(),
+              "page_visits": page_qs.count(),
+              "total_visits": qs.count(),
+              "percent": (page_qs.count()*100)/qs.count(),
+              }
+    html_template= "home.html"
+    path=request.path
+
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template, context)
