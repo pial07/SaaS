0ff903a6a2c5839509538b8bab725e306dac0d9e
pial07 <haider.powergen@gmail.com>
2025-03-26 15:09:00 +0600

    DB test

--- src/commando/tests.py
+++ src/commando/tests.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.test import TestCase
 
-# Create your tests here.
+
+class NeonDbTestCase(TestCase):
+
+    def test_db_url(self):
+        DATABASE_URL = settings.DATABASE_URL
+        self.assertIn("neon.tech", DATABASE_URL)
+
